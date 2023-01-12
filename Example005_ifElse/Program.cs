edadbfa93c9478b6fe9e4f5d801cff9cbbb32ed3
Oleg Prokopenko <cotarius@yandex.ru>
2023-01-12 23:25:46 +0300

    Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите имя пользователя: ");
+string? userName = System.Console.ReadLine();
+if(userName.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура! Это Маша!!!");
+}
+else
+{
+    System.Console.WriteLine($"Привет, {userName}");
+}
