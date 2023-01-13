2757d436497a124e631eac9f740ba569d6700115
Oleg Prokopenko <cotarius@yandex.ru>
2023-01-13 23:28:05 +0300

    modify HomeWork002

--- HomeWork/HomeWork002/Program.cs
+++ HomeWork/HomeWork002/Program.cs
@@ -1,19 +1,21 @@
 ﻿//самый лаконичный вариант
-/*string MaxAndMin(int a, int b) => (a > b) ? $"max = {a}" : $"max = {b}"; 
+string Max(int a, int b) => (a > b) ? $"max = {a}" : $"max = {b}"; 
 
+int number, number2;
 System.Console.Write("Введите первое число: ");
-while(!Int32.TryParse(Console.ReadLine(), out numberA))
+while(!Int32.TryParse(Console.ReadLine(), out number))
 {
     Console.Write("Ошибка!! Вы вводите не цифру!! Повторите ещё раз: ");
 }
 System.Console.Write("Введите второе число: ");
-while(!Int32.TryParse(Console.ReadLine(), out numberB))
+while(!Int32.TryParse(Console.ReadLine(), out number2))
 {
     Console.Write("Ошибка!! Вы вводите не цифру!! Повторите ещё раз: ");
 }
+System.Console.WriteLine(Max(number, number2));
 
 System.Console.WriteLine();
-*/
+
 //простой вариант
 int numberA, numberB;
 System.Console.Write("Введите первое число: ");
