0ebcda1a212686ad7207e2fd46d57e4bc6eb0837
Oleg Prokopenko <cotarius@yandex.ru>
2023-01-17 23:14:52 +0300

    add Homework013

---
+++ HomeWork/HomeWork013/Program.cs
@@ -0,0 +1,29 @@
+﻿void FindThirdDigit(int number)
+{    
+    int result;
+    number = Math.Abs(number);
+    if (number > 99)
+    {
+        while (number > 999)
+        {
+            number /= 10;
+        }
+        result = number % 10;
+        System.Console.WriteLine($"Третья цифра в числе: {result}");
+    }
+    else
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+int number;
+System.Console.Write("Введите любое целое число: ");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        System.Console.Write("Умоляю! Пожалуйста, введите челое число: ");
+    }
+
+FindThirdDigit(number);
+
+
