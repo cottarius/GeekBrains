676ded788132d8581f2b0f363b8e33ccb887d9b5
Oleg Prokopenko <cotarius@yandex.ru>
2023-01-28 00:40:49 +0300

    add HomeWork3in1(34,36,38)

---
+++ HomeWork/HomeWork3in1(34,36,38)/HomeWork3in1(34,36,38)/Program.cs
@@ -0,0 +1,26 @@
+﻿using HomeWork3in1_34_36_38_;
+
+EvenCount evenCount = new EvenCount();
+evenCount.PrintArray();
+Console.WriteLine($"Количество чётных чисел в массиве: {evenCount.CountEvenNumbers()}");
+Thread.Sleep(1000);
+
+
+Console.WriteLine();
+
+
+Thread.Sleep(1000);
+SumOfOddElements sumOfOddElements = new SumOfOddElements();
+sumOfOddElements.PrintArray();
+Console.WriteLine(sumOfOddElements.GetSumOfOdd());
+
+
+Console.WriteLine();
+
+
+DiffMaxMin diffMaxMin = new DiffMaxMin();
+diffMaxMin.PrintArray();
+Console.WriteLine(diffMaxMin.DiffBetweenMaxMin());
+Thread.Sleep(2000);
+Console.WriteLine("Ушел в ребут...");
+
