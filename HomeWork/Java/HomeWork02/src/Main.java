06e1d1d5968805e8edd8229e155123141d308539
Oleg Prokopenko <cotarius@yandex.ru>
2023-04-22 21:55:18 +0300

    update HomeWork/Java/HomeWork02

--- HomeWork/Java/HomeWork02/src/Main.java
+++ HomeWork/Java/HomeWork02/src/Main.java
@@ -1,4 +1,4 @@
-package src;/*
+/*
 Дана json строка [{ "фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4",
 "предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
 Задача написать метод(ы), который распарсит строку и выдаст ответ вида:
@@ -13,11 +13,12 @@ substring и т.д. по необходимости
 
 Создать метод, который запишет результат работы в файл. Обработайте исключения и запишите ошибки в лог файл.
 2. *Получить исходную json строку из файла, используя FileReader или Scanner
-3. *Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
  */
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.FileHandler;
@@ -26,32 +27,48 @@ import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Реализовал задание через библиотеку JSON (jackson) и классом Student с аннотациями
+        Сначала создал строку jsonString. Но потом переделал проект так, что json-строка достаётся из файла
+        students.json (так как эта задача есть во втором задании)
+         */
         String jsonString = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
                 "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}," +
                 "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
-
+        File file = new File("students.json");
+        String str = readFromJSON(file);
+        System.out.println(str);
+        saveToFile(str);
+    }
+    static String readFromJSON (File file) {
         ObjectMapper objectMapper = new ObjectMapper();
         StringBuilder stringBuilder = new StringBuilder();
-
+        Logger logger = Logger.getAnonymousLogger();
+        FileHandler fileHandler = null;
         try {
-            Student[] students = objectMapper.readValue(jsonString, Student[].class);
+            fileHandler = new FileHandler("log.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.addHandler(fileHandler);
+        try {
+            Student[] students = objectMapper.readValue(file, Student[].class);
             for (Student student : students) {
                 stringBuilder.append(student.getList());
                 stringBuilder.append("\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
+            logger.log(Level.WARNING, e.getMessage());
         }
-        String str = stringBuilder.toString();
-        System.out.println(str);
-        saveToFile(str);
+        fileHandler.close();
+        return stringBuilder.toString();
     }
-
     static void saveToFile(String s) {
         Logger logger = Logger.getAnonymousLogger();
         FileHandler fileHandler = null;
         try {
-            fileHandler = new FileHandler("log.txt");
+            fileHandler = new FileHandler("log.txt", true);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -79,4 +96,5 @@ class Student {
         return ("Студент " + name + " получил " + grade + " по предмету " + subject);
     }
 }
+
 
