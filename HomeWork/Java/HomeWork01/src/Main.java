20d9e9aa17f984605ba72e736764dd98619314db
Oleg Prokopenko <cotarius@yandex.ru>
2023-04-22 11:28:38 +0300

    update HomeWork/Java/HomeWork01

--- HomeWork/Java/HomeWork01/src/Main.java
+++ HomeWork/Java/HomeWork01/src/Main.java
@@ -26,10 +26,8 @@ public class Main {
     }
     //
     public static double Factorial(double n) {
-        if (n == 1)
-            return 1;
-        else if (n < 0)
-            return -1;
+        if (n == 1) return 1;
+        else if (n < 0) return -1;
         return n * Factorial(n - 1);
     }
 
