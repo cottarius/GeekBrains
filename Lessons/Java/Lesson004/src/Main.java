d5a173cc7e9139387387be8b7b48d7bdabd2e3f0
Oleg Prokopenko <cotarius@yandex.ru>
2023-05-02 22:52:57 +0300

    update  Lessons/Java/Lesson004

--- Lessons/Java/Lesson004/src/Main.java
+++ Lessons/Java/Lesson004/src/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        ex4();
+        ex0();
 
 
     }
