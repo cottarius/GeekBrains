11f52a8aa7dfd8487cf4b8efa855e95dd48f4083
Oleg Prokopenko <cotarius@yandex.ru>
2023-05-03 23:16:35 +0300

    update HomeWork/Java/HomeWork05

--- HomeWork/Java/HomeWork05/src/Main.java
+++ HomeWork/Java/HomeWork05/src/Main.java
@@ -4,6 +4,8 @@ import java.util.*;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        ex0();
+        System.out.println();
         ex1();
     }
     static void ex0(){
@@ -11,6 +13,7 @@ public class Main {
         Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько
         телефонов.
          */
+        System.out.println("Задание 1");
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.addContact("Pavel", "123456");
         phoneBook.addContact("Ivan", "456123");
@@ -30,16 +33,36 @@ public class Main {
         Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
         Отсортировать по убыванию популярности. Для сортировки использовать TreeMap.
          */
+        System.out.println("Задание 2");
         String l = "Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова,\n" +
               "Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина,\n" +
               "Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов.";
 
 
         ArrayList<String> list = prepareString(l);
-        Map<Integer, ArrayList<String>> map = prepareTreeMap(list);
-        printNames(map);
+        printDuplicateNames(list);
+        Map<Integer, ArrayList<String>> map = sortByReversOrder(list);
+        printSortedNames(map);
     }
-    static Map<Integer, ArrayList<String>> prepareTreeMap(ArrayList<String> list){
+    static void printDuplicateNames(ArrayList<String> list){
+        System.out.println("Список имён с повторениями:");
+        Map<String, Integer> map = new TreeMap<>();
+        for (String name: list) {
+            map.put(name, Collections.frequency(list, name));
+        }
+        for (Map.Entry<String, Integer> entry: map.entrySet()){
+            String key = entry.getKey();
+            int value = entry.getValue();
+            //По условию задачи повторений каждого имени меньше 10
+            if(value > 1 && value < 5) {
+                System.out.println(key + " повторяется " + value + " раза");
+            } else {
+                System.out.println(key + " повторяется " + value + " раз");
+            }
+        }
+        System.out.println();
+    }
+    static Map<Integer, ArrayList<String>> sortByReversOrder(ArrayList<String> list){
         Map<Integer, ArrayList<String>> map = new TreeMap<>(Comparator.reverseOrder());
         for (String name: list) {
             int count = Collections.frequency(list, name);
@@ -48,7 +71,6 @@ public class Main {
                 if (list2.contains(name)) {
                     continue;
                 } else {
-
                     list2.add(name);
                 }
             } else {
@@ -64,21 +86,23 @@ public class Main {
         s = s.replace(".", "");
         s = s.replace("\n", " ");
         String[] arr = s.split(",");
-        //System.out.println(Arrays.toString(arr));
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
             if ((arr[i] = arr[i].trim()).isEmpty()) continue;
             stringBuilder.append(arr[i].substring(0, arr[i].indexOf(" ")));
             stringBuilder.append(" ");
         }
-        //System.out.println(stringBuilder);
         return new ArrayList<>(List.of(stringBuilder.toString().split(" ")));
     }
-    static void printNames(Map<Integer, ArrayList<String>> map){
+    static void printSortedNames(Map<Integer, ArrayList<String>> map){
+        System.out.println("Сортировка по убыванию популярности:");
         for (Map.Entry<Integer, ArrayList<String>> entry: map.entrySet()) {
             int key = entry.getKey();
             List<String> value = entry.getValue();
-            System.out.println(value + " повторяется " + key + " раз(а) ");
+            for(String name : value){
+                System.out.println(name + " (" + key + ") ");
+            }
         }
+        System.out.println();
     }
 }
