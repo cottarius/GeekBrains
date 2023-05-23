fd1db367a939c498f53beb1064837d247a6dd515
Oleg Prokopenko <cotarius@yandex.ru>
2023-05-23 23:21:35 +0300

    add Lessons/Java/OOP_Metanit/

---
+++ Lessons/Java/OOP_Metanit/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+class Square {
+    private int width;
+    private int height;
+
+    public void setWidth()
+}
+class Rectangle extends Square {
+
+}
