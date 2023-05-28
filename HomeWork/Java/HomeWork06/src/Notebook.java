56ebee53ad668ac2276a4a5ff047123d4b0b06ee
Oleg Prokopenko <cotarius@yandex.ru>
2023-05-28 15:04:12 +0300

    update HomeWork/Java/HomeWork06/

--- HomeWork/Java/HomeWork06/src/Notebook.java
+++ HomeWork/Java/HomeWork06/src/Notebook.java
@@ -1,44 +1,52 @@
+import lombok.Getter;
+import lombok.Setter;
+
 public class Notebook {
-    private int ram;
-    private int hdd;
-    private String os;
-    private String color;
+    @Getter @Setter private int Ram;
+    @Getter @Setter private int Hdd;
+    @Getter @Setter private String Os;
+    @Getter @Setter private String Color;
+
+//    public int getRam() {
+//        return ram;
+//    }
+//    public int getHdd() {
+//        return hdd;
+//    }
+//    public String getColor() {
+//        return color;
+//    }
+//    public String getOs() {
+//        return os;
+//    }
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//    public void setHdd(int hdd) {
+//        this.hdd = hdd;
+//    }
+//    public void setOs(String os) {
+//        this.os = os;
+//    }
+//    public void setRam(int ram) {
+//        this.ram = ram;
+//    }
+
+
+
+
+
     public Notebook(int ram, int hdd, String os, String color) {
-        this.ram = ram;
-        this.hdd = hdd;
-        this.os = os;
-        this.color = color;
+        this.Ram = ram;
+        this.Hdd = hdd;
+        this.Os = os;
+        this.Color = color;
     }
 
     public Notebook() {
 
     }
-
-    public int getRam() {
-        return ram;
-    }
-    public int getHdd() {
-        return hdd;
-    }
-    public String getColor() {
-        return color;
-    }
-    public String getOs() {
-        return os;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-    public void setHdd(int hdd) {
-        this.hdd = hdd;
-    }
-    public void setOs(String os) {
-        this.os = os;
-    }
-    public void setRam(int ram) {
-        this.ram = ram;
-    }
     public void print(){
-        System.out.printf("Ram: %d, HDD: %d, Operation System: %s, Color: %s\n", ram, hdd, os, color);
+        System.out.printf("Ram: %d, HDD: %d, Operation System: %s, Color: %s\n", Ram, Hdd, Os, Color);
     }
 }
