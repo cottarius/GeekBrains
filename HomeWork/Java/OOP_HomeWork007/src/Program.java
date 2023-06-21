ee0be24a92941236f84d993819332148e0b0ebc5
Prokopenko Oleg <cotarius@yandex.ru>
2023-06-21 22:54:14 +0300

    update HomeWork\Java\OOP_HomeWork007

--- HomeWork/Java/OOP_HomeWork007/src/Program.java
+++
@@ -1,9 +0,0 @@
-import controller.Controller;
-import view.View;
-
-public class Program {
-    public static void main(String[] args) {
-        Controller controller = new Controller(new View());
-        controller.buttonClick();
-    }
-}
