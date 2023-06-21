2b79f2f2ad17156c96fbc352b9feaf5692547b7a
Prokopenko Oleg <cotarius@yandex.ru>
2023-06-21 23:46:46 +0300

    final update2 HomeWork/Java/OOP_HomeWork007

--- HomeWork/Java/OOP_HomeWork007/controller/Controller.java
+++ HomeWork/Java/OOP_HomeWork007/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import model.complex.Complex;
 import service.*;
 import view.View;
@@ -28,36 +27,43 @@ public class Controller {
             e.printStackTrace();
         }
         logger.addHandler(fileHandler);
-        Complex c1 = new Complex(view.inputValue("Re: "), view.inputValue("Im: "));
-        char operation = view.inputOperationSign("Operation Sign: ");
-        Complex c2 = new Complex(view.inputValue("Re: "), view.inputValue("Im: "));
-        switch (operation) {
-            case '+' -> {
-                ComplexOperation complexOperation = new ComplexAddition();
-                Complex result = complexOperation.mathOperation(c1, c2);
-                complexOperation.print(result);
-                logger.log(Level.INFO, c1 + " + " + c2 + " = " + result);
+        try {
+            Complex c1 = new Complex(view.inputValue("Re: "), view.inputValue("Im: "));
+
+            char operation = view.inputOperationSign("Operation Sign: ");
+
+            Complex c2 = new Complex(view.inputValue("Re: "), view.inputValue("Im: "));
+
+            switch (operation) {
+                case '+' -> {
+                    ComplexOperation complexOperation = new ComplexAddition();
+                    Complex result = complexOperation.mathOperation(c1, c2);
+                    complexOperation.print(result);
+                    logger.log(Level.INFO, c1 + " + " + c2 + " = " + result);
+                }
+                case '-' -> {
+                    ComplexOperation complexOperation = new ComplexSubstraction();
+                    Complex result = complexOperation.mathOperation(c1, c2);
+                    complexOperation.print(result);
+                    logger.log(Level.INFO, c1 + " - " + c2 + " = " + result);
+                }
+                case '*' -> {
+                    ComplexOperation complexOperation = new ComplexMultiplication();
+                    Complex result = complexOperation.mathOperation(c1, c2);
+                    complexOperation.print(result);
+                    logger.log(Level.INFO, c1 + " * " + c2 + " = " + result);
+                }
+                case '/' -> {
+                    ComplexOperation complexOperation = new ComplexDivision();
+                    Complex result = complexOperation.mathOperation(c1, c2);
+                    complexOperation.print(result);
+                    logger.log(Level.INFO, c1 + " / " + c2 + " = " + result);
+                }
+                default -> logger.log(Level.WARNING, "Unexpected sign: " + operation);
             }
-            case '-' -> {
-                ComplexOperation complexOperation = new ComplexSubstraction();
-                Complex result = complexOperation.mathOperation(c1, c2);
-                complexOperation.print(result);
-                logger.log(Level.INFO, c1 + " - " + c2 + " = " + result);
-            }
-            case '*' -> {
-                ComplexOperation complexOperation = new ComplexMultiplication();
-                Complex result = complexOperation.mathOperation(c1, c2);
-                complexOperation.print(result);
-                logger.log(Level.INFO, c1 + " * " + c2 + " = " + result);
-            }
-            case '/' -> {
-                ComplexOperation complexOperation = new ComplexDivision();
-                Complex result = complexOperation.mathOperation(c1, c2);
-                complexOperation.print(result);
-                logger.log(Level.INFO, c1 + " / " + c2 + " = " + result);
-            }
-            default -> logger.log(Level.WARNING, "Unexpected sign: " + operation);
+            fileHandler.close();
+        } catch (InputMismatchException ex) {
+            logger.log(Level.WARNING, "Введены некорректные данные при вводе комлексного числа!");
         }
-        fileHandler.close();
     }
 }
